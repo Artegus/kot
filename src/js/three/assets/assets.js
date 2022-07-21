733e3254c4952480465e15f1abd7bc631fe35954
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-07-21 20:03:00 +0200

    feat: added videos

--- src/js/three/assets/assets.js
+++ src/js/three/assets/assets.js
@@ -2,7 +2,23 @@ const assets = [
     {
         name: 'kotRoom',
         type: 'glbModel',
-        path: '/models/kot_scene.glb'
+        path: '/models/kot.glb'
+        // path: '/models/kot_scene_v3_1.glb'
+    },
+    {
+        name: 'kot_water_0',
+        type: 'videoTexture',
+        path: '/textures/kot_water_0.webm'
+    },
+    {
+        name: 'kot_water_1',
+        type: 'videoTexture',
+        path: '/textures/kot_water_1.webm'
+    },
+    {
+        name: 'kots',
+        type: 'videoTexture',
+        path: '/textures/kots.webm'
     }
 ]
 
